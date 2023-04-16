183a7bb74a2b4d957b89ce96ee052b609b54d4fd
Hayato <hayatokoyama@users.noreply.github.com>
2023-04-16 13:06:11 -0700

    moved DB connection to db.js so that it can be accessible from any file

--- server/app.js
+++ server/app.js
@@ -5,63 +5,15 @@ const port = process.env.PORT || 8000;
 
 const cors = require("cors");
 
-//View Engine
-app.set("view engine", "ejs");
-
 app.use(
   cors({
     origin: "*",
   })
 );
-//Render Home Page and
-app.get("/", (req, res) => {
-  res.render("pages/index");
-});
 
-// Insert a user into the user table in the mySQL.
-// app.get("/add", (req, res) => {
-//   connection.query(
-//     "INSERT INTO user (name, email, password, isArtist) VALUES ('Takuya9', 'takuya@gmail.com', 'hayato', 'true' )",
-//     (error, resFromInsert) => {
-//       if (error) throw error;
-
-//       if (!error) {
-//         console.log(res);
-//       }
-//     }
-//   );
-// });
-
-//The desc is a MySQL reserved word, therefore you cannot use it.
-// app.get("/post", (req, res) => {
-//   console.log("hey what up???");
-//   connection.query(
-//     "INSERT INTO post (artistId, title,price,setsumei,desc2) VALUES (2, 'testClass2',50,'this is setsumei','desc2')",
-//     (error, res2) => {
-//       if (error) throw error;
-
-//       if (!error) {
-//         console.log("succeed");
-//         res.send("succeed");
-//       }
-//     }
-//   );
-// });
-const queryRoute = require("./routes/queries");
+const queryRoute = require("./routes/userQueries");
 app.use("/api/query", queryRoute);
 
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
 // module.connection = connection;
-
-/**
- * below is an example of Select query.
- */
-// connection.query("SELECT *  FROM user ", (err, rows) => {
-//   if (err) throw err;
-
-//   if (!err) {
-//     console.log(rows);
-//     res.render("pages/index", { rows });
-//   }
-// });
--- server/routes/userQueries.js
+++ server/routes/userQueries.js
@@ -1,14 +1,5 @@
 const router = require("express").Router();
-const mysql = require("mysql");
-// const connection = require("../app/connection");
-
-//MySQL Connection Detail
-const connection = mysql.createConnection({
-  host: "us-cdbr-east-06.cleardb.net",
-  user: "b463dda7a021ae",
-  password: "af90497c",
-  database: "heroku_85a07a97c5525a4",
-});
+const connection = require("../db");
 
 router.get("/post", (req, res) => {
   console.log("hey what up???");
